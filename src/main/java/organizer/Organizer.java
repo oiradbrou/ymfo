ec5a1e1f50dd666b43fbfd2e0c216f75e46c81c4
Raflat <adouby01@gmail.com>
2022-05-27 21:31:17 +0200

    Correction: removed trailing white spaces

--- src/main/java/organizer/Organizer.java
+++ src/main/java/organizer/Organizer.java
@@ -13,65 +13,65 @@ import dirchooser.DirChooser;
  * second one composed of directories indicating the month of the file.
  * Furthermore, the files are enumerated from 0 to n, according to how many
  * of them are contained in the directory.
- * 
+ *
  * @author raflat
  */
 public final class Organizer {
-	
+
 	private YMFileDirectory srcDir;
 	private YMDirectory destDir;
-	
+
 	/**
-	 * Method that runs the {@link Organizer} by launching the 
+	 * Method that runs the {@link Organizer} by launching the
 	 * directory selection module.
 	 */
 	public static void run() {
 		Organizer organizer = new Organizer();
 		DirChooser.chooseAndOrganizeWith(organizer);
 	}
-	
+
 	/**
 	 * Setter to set the source {@link OFileDirectory} from which
 	 * the files to organize are taken, given the path to a
 	 * directory.
-	 * 
+	 *
 	 * @param srcDirPath - Path to a directory.
-	 * @throws NotDirectoryException 
+	 * @throws NotDirectoryException
 	 */
 	public void setSrcDir(String srcDirPath) throws NotDirectoryException {
 		srcDir = YMFileDirectory.createFromDir(srcDirPath);
 	}
-	
+
 	/**
 	 * Setter to set the destination {@link StoreDirectory} to which
 	 * the files to organize are stored, given the path to a
 	 * directory.
-	 * 
+	 *
 	 * @param destDirPath - Path to a directory.
 	 */
 	public void setDestDir(String destDirPath) {
 		destDir = YMDirectory.createFromDir(destDirPath);
 	}
-	
+
 	/**
-	 * Method that organizes the files contained in the source 
+	 * Method that organizes the files contained in the source
 	 * {@link OFileDirectory} by renaming them to their new location.
 	 */
 	public void renameFiles() {
 		LinkedList<String> alreadyPresentDates = destDir.containedDates();
-		
+
 		for (YMFile image : srcDir.listFiles()) {
 			String imageYear = image.extractYear();
 			String imageMonth = image.extractMonth();
 			String imageName = imageYear + "-" + imageMonth;
-					
+
 			if (!alreadyPresentDates.contains(imageName))
 				destDir.createDirectory(imageYear, imageMonth);
-			
+
 			imageName += "_" + destDir.lastImageNumber(imageYear, imageMonth);
-			
+
 			destDir.storeFile(image);
 		}
 	}
-	
+
 }
