9099798b9439812d68e573368ba7e4b1eed015e8
Raflat <adouby01@gmail.com>
2022-05-27 21:34:08 +0200

    Correction: removed trailing white spaces

--- src/main/java/organizer/YMFile.java
+++ src/main/java/organizer/YMFile.java
@@ -9,7 +9,7 @@ import java.nio.file.attribute.BasicFileAttributes;
 /**
  * Class that wraps a file of {@link File} type.<br>
  * Provides methods to extract it's date.
- *  
+ *
  * @author raflat
  */
 final class YMFile {
@@ -20,29 +20,29 @@ final class YMFile {
 	/**
 	 * Static factory method that given the path to a file creates
 	 * it's corresponding OFile.
-	 * 
+	 *
 	 * @param filePath - String of path to a file.
-	 * @throws NotFileException 
+	 * @throws NotFileException
 	 */
 	static YMFile createFromPath(String filePath) throws FileNotFoundException {
 		return new YMFile(filePath);
 	}
 
 	/**
-	 * Getter that returns the {@link File} that's wrapped 
+	 * Getter that returns the {@link File} that's wrapped
 	 * by the {@link OFile} it's called on.
-	 * 
+	 *
 	 * @return File associated to the OFile.
 	 */
 	File getFile() {
 		return file;
 	}
-	
+
 	/**
-	 * Method that extracts the year of the {@link OFile}'s 
+	 * Method that extracts the year of the {@link OFile}'s
 	 * date it's called on.
-	 * 
-	 * @return 
+	 *
+	 * @return
 	 * String representing the number of the year of the
 	 * OFile it's called on.<br>
 	 * The format of the returned string is "yyyy".
@@ -50,41 +50,41 @@ final class YMFile {
 	String extractYear() {
 		return date.substring(0, 4);
 	}
-	
+
 	/**
-	 * Method that extracts the month of the {@link OFile}'s 
+	 * Method that extracts the month of the {@link OFile}'s
 	 * date it's called on.
-	 * 
+	 *
 	 * @return
-	 * String representing the number of the month of the OFile 
+	 * String representing the number of the month of the OFile
 	 * it's called on.<br>
 	 * The format of the returned string is "mm".
 	 */
 	String extractMonth() {
 		return date.substring(5, 7);
 	}
-		
+
 	/**
-	 * Method that extracts the extension of the {@link OFile}, 
+	 * Method that extracts the extension of the {@link OFile},
 	 * preceded by a dot.
-	 * 
+	 *
 	 * @return String in the form ".extension".
 	 */
 	String extractDotExtension() {
 		String name = file.getName();
 		return name.substring(name.indexOf("."));
 	}
-	
+
 	private YMFile(String path) throws FileNotFoundException {
 		file = new File(path);
-		
+
 		if (!file.isFile())
 			throw new FileNotFoundException("Error trying to use the file at " + path);
-			
+
 		date = extractDate();
 	}
 
-	private String extractDate()  {
+	private String extractDate() {
 		String date = null;
 		try {
 			date = Files.readAttributes(file.toPath(), BasicFileAttributes.class)
