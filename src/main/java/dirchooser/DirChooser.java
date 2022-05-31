04f653ab269fa3d8fccb3bdbc6c9abb9757a5115
Raflat <adouby01@gmail.com>
2022-05-31 21:55:09 +0200

    update: refactoring

--- src/main/java/dirchooser/DirChooser.java
+++ src/main/java/dirchooser/DirChooser.java
@@ -1,5 +1,6 @@
 package dirchooser;
 
+import java.awt.Component;
 import java.awt.GridBagConstraints;
 import java.awt.Image;
 import java.awt.Insets;
@@ -29,7 +30,9 @@ public final class DirChooser extends JFrame implements ActionListener, Configur
 	private Button destButton;
 	private Button runButton;
 
-	private YMOrganizer organizer;
+	private final YMOrganizer organizer;
+	
+	GridBagConstraints  contrains = new GridBagConstraints();
 
 	public static DirChooser chooseAndOrganizeWith(YMOrganizer organizer) {
 		return new DirChooser(organizer);
@@ -84,10 +87,8 @@ public final class DirChooser extends JFrame implements ActionListener, Configur
 	}
 
 	private void initFrame() {
-		initPanel();
-		initLabels();
-		initTextFields();
-		initButtons();
+		createComponents();
+		addButtonsToActionListener();
 
 		CIni iniFile = CIni.createFromIni("src\\main\\resources\\conf.ini");
 		customizeComponents(iniFile);
@@ -97,29 +98,21 @@ public final class DirChooser extends JFrame implements ActionListener, Configur
 		this.pack();
 		this.setVisible(true);
 	}
-
-	private void initPanel() {
+	
+	private void createComponents() {
 		panel = Panel.create();
-	}
-
-	private void initLabels() {
 		srcLabel = Label.withText("Source folder:");
 		destLabel = Label.withText("Destination folder:");
-	}
-
-	private void initTextFields() {
 		srcTextField = TextField.create();
 	  	destTextField = TextField.create();
+	  	srcButton = Button.withText("Browse");
+		destButton = Button.withText("Browse");
+		runButton = Button.withText("Run");
 	}
 
-	private void initButtons() {
-		srcButton = Button.withText("Browse");
+	private void addButtonsToActionListener() {
 		srcButton.addActionListener(this);
-
-		destButton = Button.withText("Browse");
 		destButton.addActionListener(this);
-
-		runButton = Button.withText("Run");
 		runButton.addActionListener(this);
 	}
 
@@ -136,30 +129,21 @@ public final class DirChooser extends JFrame implements ActionListener, Configur
 	}
 
 	private void attachComponents() {
-		GridBagConstraints  contrains = new GridBagConstraints();
 		contrains.insets = new Insets(10, 10, 10, 10);
-		contrains.gridx = 0;
-		contrains.gridy = 0;
-		panel.add(srcLabel, contrains);
-		contrains.gridx = 1;
-		contrains.gridy = 0;
-		panel.add(srcTextField, contrains);
-		contrains.gridx = 2;
-		contrains.gridy = 0;
-		panel.add(srcButton, contrains);
-		contrains.gridx = 0;
-		contrains.gridy = 1;
-		panel.add(destLabel, contrains);
-		contrains.gridx = 1;
-		contrains.gridy = 1;
-		panel.add(destTextField, contrains);
-		contrains.gridx = 2;
-		contrains.gridy = 1;
-		panel.add(destButton, contrains);
-		contrains.gridx = 1;
-		contrains.gridy = 3;
+		addXYConstrained(0, 0, srcLabel);
+		addXYConstrained(1, 0, srcTextField);
+		addXYConstrained(2, 0, srcButton);
+		addXYConstrained(0, 1, destLabel);
+		addXYConstrained(1, 1, destTextField);
+		addXYConstrained(2, 1, destButton);
 		contrains.fill = GridBagConstraints.HORIZONTAL;
-		panel.add(runButton, contrains);
+		addXYConstrained(1, 3, runButton);
+	}
+	
+	private void addXYConstrained(int gridX, int gridY, Component component) {
+		contrains.gridx = gridX;
+		contrains.gridy = gridY;
+		panel.add(component, contrains);
 	}
 
 }
