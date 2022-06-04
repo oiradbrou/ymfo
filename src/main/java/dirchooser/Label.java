070edc7f51dfdd52eac319e89e29c173d3520f42
Raflat <adouby01@gmail.com>
2022-06-04 20:10:43 +0200

    update: javadoca added to class

--- src/main/java/dirchooser/Label.java
+++ src/main/java/dirchooser/Label.java
@@ -4,14 +4,33 @@ import java.awt.Color;
 
 import javax.swing.JLabel;
 
+/**
+ * Class that extends the {@link JLabel} type by providing it with a method to
+ * configure it according to a {@link CIni} configuration file; this method is
+ * provided by the interface {@link Configurable}.
+ *
+ * @author Dario
+ *
+ */
 final class Label extends JLabel implements Configurable {
 
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * Static factory method to create a {@link Label} object displaying a
+	 * given text.
+	 *
+	 * @param text - {@link String} of text.
+	 * @return New Label instance.
+	 */
 	static Label withText(String text) {
 		return new Label(text);
 	}
 
+	/**
+	 * Method that configures the instance of {@link Label} is called on. The
+	 * configuration is taken from the {@link CIni} file provided.
+	 */
 	@Override
 	public void configure(CIni iniFile) {
 		setForeground(Color.decode(iniFile.readFrom("Label", "TextColor")));		
