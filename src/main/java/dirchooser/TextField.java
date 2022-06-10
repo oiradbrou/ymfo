f982ff8f232481e05f7a194a0f4178c41b8f00d9
Raflat <adouby01@gmail.com>
2022-06-10 02:41:38 +0200

    update: javadoc added to class

--- src/main/java/dirchooser/TextField.java
+++ src/main/java/dirchooser/TextField.java
@@ -2,14 +2,28 @@ package dirchooser;
 
 import javax.swing.JTextField;
 
+/**
+ * Class that extends the {@link JTextField} type by providing it with a method
+ * to configure it according to a {@link CIni} configuration file; this method
+ * is provided by the interface {@link Configurable}.
+ */
 final class TextField extends JTextField implements Configurable {
 
 	private static final long serialVersionUID = 1L;
 	
+	/**
+	 * Static factory method to create a {@link TextField} object.
+	 *
+	 * @return New TextField instance.
+	 */
 	static TextField create() {
 		return new TextField();
 	}
 	
+	/**
+	 * Method that configures the instance of {@link TextField} is called on.
+	 * The configuration is taken from the {@link CIni} file provided.
+	 */
 	@Override
 	public void configure(CIni iniFile) {
 		setColumns(Integer.parseInt(iniFile.readFrom("TextField", "Length")));
