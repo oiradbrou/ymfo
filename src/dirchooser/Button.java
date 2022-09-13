895f91ad0657e6289c40c3e53a5c20516e664ff2
unknown <adouby01@gmail.com>
2022-09-13 15:48:59 +0200

    updated comments

--- src/dirchooser/Button.java
+++ src/dirchooser/Button.java
@@ -5,31 +5,33 @@ import java.awt.Color;
 import javax.swing.JButton;
 
 /**
- * Class that extends the {@link JButton} type by providing it with a method to
- * configure it according to a {@link CIni} configuration file; this method is
- * provided by the interface {@link Configurable}.
+ * Extension of a {@link JButton} that sets the object's background and text to a default color. It also
+ * provides a method to change the button's text.
  */
 final class Button extends JButton {
 
-	private static final long serialVersionUID = 1L;
-
 	/**
-	 * Static factory method to create a {@link Button} object displaying a
-	 * given text.
-	 *
-	 * @param text - {@link String} of text.
-	 * @return New Button instance.
+	 * Default serial version ID to avoid warnings.
 	 */
-	static Button withText(String text) {
-		return new Button(text);
-	}
-
+	private static final long serialVersionUID = 1L;
+	
 	private Button(String text) {
 		super(text);
 
 		setBackground(Color.decode("#808080"));
 		setForeground(Color.decode("#FFFFFF"));
 		setFocusable(false);
+	}
+
+	/**
+	 * Create a {@link Button} object displaying the text provided.
+	 *
+	 * @param text - {@link String} of text.
+	 * 
+	 * @return New Button instance.
+	 */
+	static Button withText(String text) {
+		return new Button(text);
 	}
 
 }
