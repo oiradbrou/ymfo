4aff01d6ec522de0878f9b7877b19d48fb1e8d1d
unknown <adouby01@gmail.com>
2022-09-13 15:40:24 +0200

    updated comments

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,12 +3,12 @@ package main;
 import organizer.YMOrganizer;
 
 /**
- * Class that only contains the main method.
+ * Main class. Contains solely the main method that allows the application to be executed.
  */
 public class Main {
 
 	/**
-	 * Main method, used just to start the program. 
+	 * Main method. Only used to run the application. 
 	 */
 	public static final void main(String args[]) {
 		YMOrganizer.run();
