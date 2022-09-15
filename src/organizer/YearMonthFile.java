35c83ec91cf5330ac1b9587c859e267d9c0d1444
unknown <adouby01@gmail.com>
2022-09-15 18:49:55 +0200

    various changes

--- src/organizer/YearMonthFile.java
+++ src/organizer/YearMonthFile.java
@@ -8,8 +8,7 @@ import java.nio.file.attribute.BasicFileAttributes;
 
 /**
  * Class that wraps a file, providing methods to extract it's date and change it's path.<br>
- * The date extracted is the one referring to the last time the file was
- * modified.
+ * The date extracted is the one referring to the last time the file was modified.
  */
 final class YearMonthFile {
 
@@ -17,81 +16,87 @@ final class YearMonthFile {
 	private final String date;
 
 	/**
-	 * Getter that return the path of the {@link YearMonthFile} it's called on.
+	 * Given the path to a file, creates it's corresponding {@link YearMonthFile} and returns it.
 	 *
-	 * @return Path as a {@link String}.
+	 * @param path - {@link String} path to a file.
 	 */
-	String getPath() {
+	static YearMonthFile from(String path) throws FileNotFoundException {
+		return new YearMonthFile(path);
+	}
+
+	/**
+	 * Returns the extension of the {@link YearMonthFile} it's called on, preceded by a dot.
+	 *
+	 * @return {@link String} representing the extension as <b>".extension"</b>.
+	 */
+	String dotExtension() {
+		String name = file.getName();
+
+		return name.substring(name.indexOf("."));
+	}
+
+	/**
+	 * Returns the path of the {@link YearMonthFile} it's called on.
+	 *
+	 * @return {@link String} representing the path of the file.
+	 */
+	String path() {
 		return file.getPath();
 	}
 
 	/**
-	 * Static factory method that, given the path to a file, creates it's corresponding {@link YearMonthFile}
-	 * and returns it.
+	 * Returns the month's number of the {@link YearMonthFile} date it's called on.
 	 *
-	 * @param filePath - {@link String} of path to a file.
-	 * @throws FileNotFoundException if <b>filePath</b> leads to a resource that isn't a file.
-	 */
-	static YearMonthFile createFromPath(String filePath) throws FileNotFoundException {
-		return new YearMonthFile(filePath);
-	}
-
-	/**
-	 * Method that extracts the year's number of the {@link YearMonthFile} it's called
-	 * on.
-	 *
-	 * @return Year as {@link String} formatted like "yyyy".
-	 */
-	String year() {
-		return date.substring(0, 4);
-	}
-
-	/**
-	 * Method that extracts the month's number of the {@link YearMonthFile} date it's called on.
-	 *
-	 * @return Month as {@link String} formatted like "mm".
+	 * @return Month as {@link String} as <b>"mm"</b>.
 	 */
 	String month() {
 		return date.substring(5, 7);
 	}
 
 	/**
-	 * Method that extracts the extension of the {@link YearMonthFile} it's called on, preceded by a dot.
+	 * Moves the {@link YearMonthFile} to the location pointed by the provided path.<br>
 	 *
-	 * @return {@link String} in the form ".extension".
+	 * @param path - {@link String} representing the destination.
 	 */
-	String dotExtension() {
-		String name = file.getName();
-		return name.substring(name.indexOf("."));
+	void moveTo(String path) {
+		file.renameTo(new File(path));
 	}
 
 	/**
-	 * Method that changes the {@link YearMonthFile}'s path to the provided one.<br>
+	 * Returns the year's number of the {@link YearMonthFile} it's called on.
 	 *
-	 * @param newPath - {@link String} representing the new path.
+	 * @return Year as {@link String} formatted like <b>"yyyy"</b>.
 	 */
-	void changePathTo(String newPath) {
-		file.renameTo(new File(newPath));
+	String year() {
+		return date.substring(0, 4);
 	}
 
-	private YearMonthFile(String path) throws FileNotFoundException {
+	/**
+	 * Returns the date of the {@link #file} field.<br>
+	 * Shows as message in standard error if an {@link IOException} happens while extracting the date.
+	 *  
+	 * @return Date as {@link String}.
+	 */
+	private String date() {
+		String date = null;
+
+		try {
+			date = Files.readAttributes(file.toPath(), BasicFileAttributes.class)
+					.lastModifiedTime().toString();
+		} catch (IOException exception) {
+			 System.err.println(exception.getMessage());
+		}
+
+		return date;
+	}
+
+	private YearMonthFile(String path) {
 		file = new File(path);
 
 		if (!file.isFile())
-			throw new FileNotFoundException("Error trying to use the file at " + path);
+			System.err.println("Provided path doesn't lead to a file.");
 
 		date = date();
-	}
-
-	private String date() {
-		String date = null;
-		try {
-			date = Files.readAttributes(file.toPath(), BasicFileAttributes.class)
-								.lastModifiedTime().toString();
-		} catch (IOException e) {
-			 System.err.println("Error test file.");
-		}
-		return date;
 	}
 
 }
