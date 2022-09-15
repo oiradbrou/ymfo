be3e197dadb61c1ad28bbdff7c6eb82034bb06a5
unknown <adouby01@gmail.com>
2022-09-15 19:01:41 +0200

    refactoring and updates

--- src/organizer/SourceDirectory.java
+++ src/organizer/SourceDirectory.java
@@ -2,14 +2,14 @@ package organizer;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 /**
- * Class that represents the directory from which the files that the user wants to organize are taken.<br>
- * Once initialized, the class creates a {@link YearMonthFile} for each of the files
- * it contains.
+ * Directory from which the files to organize are taken.<br>
+ * Once instantiated, the objects creates a {@link YearMonthFile} for each file it contains.
  */
 final class SourceDirectory extends Directory {
 
@@ -20,29 +20,34 @@ final class SourceDirectory extends Directory {
 	 * 
 	 * @return New SrcDirectory object.
 	 */
-	static SourceDirectory createFromDir(String dirPath) {
+	static SourceDirectory from(String dirPath) {
 		return new SourceDirectory(dirPath);
 	}
 
 	/**
-	 * Getter that returns a {@link List} of all the files contained in the {@link SourceDirectory} as
+	 * Returns a {@link List} of all the files contained in the {@link SourceDirectory} as
 	 * {@link YearMonthFile} objects.
 	 *
-	 * @return List of files.
+	 * @return {@link List} of files.
 	 */
-	List<YearMonthFile> getFiles() {
-		return Arrays.asList(dir.listFiles())
-					 .parallelStream()
-					 .map(File::getPath)
-					 .map(path -> {
-						 	try {
-						 		return YearMonthFile.createFromPath(path);
-						 	} catch (FileNotFoundException e) {
-						 		System.err.println("Ensure that the directory only contains files.");
-						 		return null;}})
-					 .collect(Collectors.toList());
+	List<YearMonthFile> files() {
+		return Arrays.asList(dir.listFiles()).parallelStream()
+				.map(File::getPath)
+				.map(path -> {
+					try {
+						return YearMonthFile.from(path);
+					} catch (FileNotFoundException exception) {
+						System.err.println("Ensure that the directory only contains files.");
+						return null;
+					}
+				}).collect(Collectors.toList());
 	}
 
+	/**
+	 * Returns a {@link SourceDirectory} that wraps the directory at the provided path.
+	 * 
+	 * @param path - {@link String} representing the path of the directory to wrap.
+	 */
 	private SourceDirectory(String path) {
 		super(path);
 	}
